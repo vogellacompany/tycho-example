86f352967fdc9dffb7df1a9c46117289ce229842
Lars Vogel <Lars.Vogel@gmail.com>
2014-08-12 14:31:23 +0200

    Tycho book examples initial upload

---
+++ com.vogella.tycho.unittests/src/com/vogella/tycho/unittests/ExampleTest.java
@@ -0,0 +1,15 @@
+package com.vogella.tycho.unittests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+	@Test
+	public void test() {
+		// just an example
+		assertTrue(true);
+	}
+
+}
