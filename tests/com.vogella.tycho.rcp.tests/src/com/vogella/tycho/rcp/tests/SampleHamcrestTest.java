bcba536c076f56333cbf5bc269050d96eb1b8dbc
Simon Scholz <simon.scholz@vogella.com>
2016-08-11 18:23:30 +0200

    Added additional hamcrest tests
    Change-Id: Ida1402198e7764499e741d5594b0792613414cc9
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- tests/com.vogella.tycho.rcp.tests/src/com/vogella/tycho/rcp/tests/SampleHamcrestTest.java
+++ tests/com.vogella.tycho.rcp.tests/src/com/vogella/tycho/rcp/tests/SampleHamcrestTest.java
@@ -1,5 +1,7 @@
 package com.vogella.tycho.rcp.tests;
 
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.arrayWithSize;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.greaterThan;
@@ -17,24 +19,38 @@ public class SampleHamcrestTest {
 	@Test
 	public void hasSizeOf3() {
 		List<Integer> list = Arrays.asList(5, 2, 4);
-		
+
 		assertThat(list, hasSize(3));
 	}
-	
+
 	@Test
 	public void containsNumbersInAnyOrder() {
 		List<Integer> list = Arrays.asList(5, 2, 4);
-		
+
 		assertThat(list, containsInAnyOrder(2, 4, 5));
 	}
-	
+
 	@Test
 	public void everyItemGreaterThan1() {
 		List<Integer> list = Arrays.asList(5, 2, 4);
-		
+
 		assertThat(list, everyItem(greaterThan(1)));
 	}
-	
+
+	@Test
+	public void arrayHasSizeOf4() {
+		Integer[] ints = new Integer[] { 7, 5, 12, 16 };
+
+		assertThat(ints, arrayWithSize(4));
+	}
+
+	@Test
+	public void arrayContainsNumbersInGivenOrder() {
+		Integer[] ints = new Integer[] { 7, 5, 12, 16 };
+
+		assertThat(ints, arrayContaining(7, 5, 12, 16));
+	}
+
 	@Test
 	public void test() {
 		assertThat("Hamcrest", is("Hamcrest"));
