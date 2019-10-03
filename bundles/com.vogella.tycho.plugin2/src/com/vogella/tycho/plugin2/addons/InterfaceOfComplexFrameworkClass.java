6f6e014411ed71f96841803e74aec9175f56d5aa
Christoph Läubrich <christoph.laeubrich@lablicate.com>
2019-10-03 15:54:39 +0200

    Add example that uses Eclipse-PlatformFilter

---
+++ bundles/com.vogella.tycho.plugin2/src/com/vogella/tycho/plugin2/addons/InterfaceOfComplexFrameworkClass.java
@@ -0,0 +1,7 @@
+package com.vogella.tycho.plugin1.addons;
+
+public interface InterfaceOfComplexFrameworkClass {
+	String getFoo();
+	
+	boolean isBar();
+}
---
+++ bundles/com.vogella.tycho.plugin2/src/com/vogella/tycho/plugin2/addons/Plugin1Addon.java
@@ -0,0 +1,22 @@
+ 
+package com.vogella.tycho.plugin1.addons;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.core.di.extensions.EventTopic;
+import org.eclipse.e4.ui.workbench.UIEvents;
+import org.osgi.service.event.Event;
+
+@SuppressWarnings("restriction")
+public class Plugin1Addon {
+
+	@Inject
+	@Optional
+	public void applicationStarted(
+			@EventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) Event event, @Optional InterfaceOfComplexFrameworkClass complexFrameworkClass) {
+		if(complexFrameworkClass != null) {
+			System.out.println("App startet: " + complexFrameworkClass.getFoo());
+		}
+	}
+}
