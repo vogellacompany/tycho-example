dbb5825b185dc0526ba3c5b9af04f80add4103a3
Lars Vogel <Lars.Vogel@vogella.com>
2022-02-28 17:26:00 +0100

    Usage of Hamcrest in test

--- com.vogella.tycho.rcp.tests/src/com/vogella/tycho/rcp/tests/SampleHamcrestTest.java
+++ com.vogella.tycho.rcp.tests/src/com/vogella/tycho/rcp/tests/SampleHamcrestTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
