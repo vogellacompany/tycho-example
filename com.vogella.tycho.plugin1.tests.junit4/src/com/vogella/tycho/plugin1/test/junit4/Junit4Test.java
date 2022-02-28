91393b6d409fb15b2719c5748f7dbee1a926f623
Lars Vogel <Lars.Vogel@vogella.com>
2022-02-28 17:51:24 +0100

    Adds a Junit4 example

---
+++ com.vogella.tycho.plugin1.tests.junit4/src/com/vogella/tycho/plugin1/test/junit4/Junit4Test.java
@@ -0,0 +1,19 @@
+package com.vogella.tycho.plugin1.test.junit4;
+
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.vogella.tycho.plugin1.addons.Plugin1Addon;
+
+public class Junit4Test {
+
+
+	@Test
+	public void verifyCallTest() {
+		Plugin1Addon plugin1Addon = new Plugin1Addon();
+
+		assertNotNull(plugin1Addon);
+	}
+}
